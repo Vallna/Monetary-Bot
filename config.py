5eac2a7792ff34c8bb45f75118d39d55c888bb9b
Vallna <valentin4688@gmail.com>
2020-12-10 21:41:56 +0200

    Первая работающая версия

---
+++ config.py
@@ -0,0 +1,11 @@
+import telebot
+
+TOKEN = '1433792399:AAGtCvdcTLmAbEIxCLrJU6dIYXEoEsv90Sk'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфериум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
